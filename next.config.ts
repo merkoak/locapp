08dfe97289ddc712c3f39b426eb6b08585c3e439
merkoak <akcaymrtt@gmail.com>
2025-12-03 03:09:43 +0300

    Final

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,8 @@
+import { type NextConfig } from 'next'; // NextConfig tipi buraya eklendi
+
 const nextConfig: NextConfig = {
+  // IBM-Watson için gerekli dış paket tanımı
   serverExternalPackages: ['ibm-watson'],
 };
+
+export default nextConfig;
