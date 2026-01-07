7067704d6c16188141d5e27e2f2d3758e94297b2
merkoak <akcaymrtt@gmail.com>
2026-01-07 22:33:59 +0300

    SEO updates-2.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://locai.ai";
+  const baseUrl = "https://locapp.ai";
 
   return [
     {
